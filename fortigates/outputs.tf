baca1630fdfeb8500f789a2aec7bdd05fb35d1a1
Bartek Moczulski <12998104+bartekmo@users.noreply.github.com>
2024-07-26 16:56:48 +0200

    tf fmt

--- fortigates/outputs.tf
+++ fortigates/outputs.tf
@@ -5,9 +5,9 @@ output "fgtha" {
 */
 
 output "mgmt_eips" {
-    value = module.fgtha.fgt_mgmt_eips
+  value = module.fgtha.fgt_mgmt_eips
 }
 
 output "default_password" {
-    value = module.fgtha.fgt_password
+  value = module.fgtha.fgt_password
 }
